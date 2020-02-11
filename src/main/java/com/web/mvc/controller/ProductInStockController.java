da8f51526b42d3d7ea5a13c1e86240d6e267a6ec
vincenttuan <vincentjava@yahoo.com.tw>
2020-02-11 15:58:58 +0800

    Derby

--- src/main/java/com/web/mvc/controller/ProductInStockController.java
+++ src/main/java/com/web/mvc/controller/ProductInStockController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductInStockController {
@@ -13,6 +14,7 @@ public class ProductInStockController {
     @Autowired
     private ProductDao dao;
     
+    @RequestMapping("/product_in_stock")
     public String inStock(Model model) {
         List<Product> products = dao.queryProduct();
         model.addAttribute("products", products);
