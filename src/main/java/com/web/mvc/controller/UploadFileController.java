da843aeb1c8ed7d075c1fef7db273409d5222c50
vincenttuan <vincentjava@yahoo.com.tw>
2020-02-25 17:35:02 +0800

    Java

--- src/main/java/com/web/mvc/controller/UploadFileController.java
+++ src/main/java/com/web/mvc/controller/UploadFileController.java
@@ -1,5 +1,12 @@
 package com.web.mvc.controller;
 
+import com.web.mvc.entity.Member;
+import com.web.mvc.repository.spec.LoginDao;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +19,9 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/upload")
 public class UploadFileController {
     
+    @Autowired
+    LoginDao dao;
+    
     @GetMapping("/")
     public String input() {
         return "uploadfile";
@@ -20,6 +30,46 @@ public class UploadFileController {
     @PostMapping("/submit")
     @ResponseBody
     public String submit(@RequestParam("file") MultipartFile file) {
+        StringBuilder sb = new StringBuilder();
+        File f = null;
+        try {
+            f = File.createTempFile("tmp", null);
+            file.transferTo(f);
+            f.deleteOnExit(); //使用完成刪除檔案
+            FileReader fr = new FileReader(f);
+            
+            char[] buffer = new char[1];
+            while (fr.read(buffer) != -1) {                
+                sb.append(buffer);
+            }
+            
+            // 將字串透過 \n 切分
+            String[] rows = sb.toString().split("\n");
+            for(String row : rows) {
+                System.out.println(row);
+                // a01,aaa,a01@java.com,1
+                String[] array = row.split(",");
+                String username = array[0].trim();
+                String password = array[1].trim();
+                String email = array[2].trim();
+                Integer priority = Integer.valueOf(array[3].trim());
+                
+                Member member = new Member();
+                member.setUsername(username);
+                member.setPassword(password);
+                member.setEmail(email);
+                member.setPriority(priority);
+                member.setCode(Integer.toHexString(member.hashCode()));
+                member.setPass(Boolean.TRUE);
+                
+                // 存入資料庫
+                dao.save(member);
+                
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "Upload OK !";
     }
     
